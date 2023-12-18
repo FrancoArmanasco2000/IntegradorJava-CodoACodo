276eb074ce0bd7f1376e6be28cc61377ad05f328
FrancoArmanasco2000 <programmingarmanasco@gmail.com>
2023-12-18 12:24:58 -0300

    Agregacion del proyecto JAVA WEB

---
+++ TpIntegrador/src/main/java/com/trabajo_integrador_codo_a_codo/tpintegrador/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.trabajo_integrador_codo_a_codo.tpintegrador;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
